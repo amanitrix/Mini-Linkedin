9ec366c255b1ea64dc802d87fc29c2b923fcbe6e
Aman <one.aman99@gmail.com>
2025-08-06 03:50:30 +0530

    update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? process.env.FRONTEND_URL
+    ? true // Allow all origins in production (or you can specify specific domains)
     : ['http://localhost:3000'],
   credentials: true
 }));
